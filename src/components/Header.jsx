515b9b6064ee515675c458ca21616822f3cda729
Martin Flischman <26260699+martinflischman@users.noreply.github.com>
2026-05-28 12:34:34 +0200

    feat: add header component
    - create App component in App.jsx
    - create src/components folder
    - add Header component with logo image and site title
    - style navbar with primary brand color, centered layout, and shadow
    - clear default README content

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div className="navbar bg-primary shadow-sm flex justify-center gap-3">
+      <img className="w-8 rounded-full" src="/globe.png" alt="world globe" />
+      <a className="text-white text-xl">my travel journal.</a>
+    </div>
+  );
+}
